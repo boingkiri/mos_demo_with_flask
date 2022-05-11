b1fce9c65d62141a56d5c46cc386b6484bb6b907
jjlee <djfelrl11@ncsoft.com>
2022-05-11 06:30:24 -0700

    Add post, get api

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=('GET', 'POST'))
 def index():
-    return render_template(
-        'index.html',
-        user='반원',
-        data={
+    data={
             'level': 60,
             'point': 360,
             'exp': 45000
         }
+    if request.method == "POST":
+        user = request.form.get('user')
+        print(user)
+    elif request.method == 'GET':
+        print('GETGET')
+        user = "반원웡원"
+    return render_template(
+        'index.html',
+        user=user,
+        data=data
     )
 
 if __name__=="__main__":
